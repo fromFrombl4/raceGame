4368800f6c924b01e21c79d0a33507e13d9842ad
roman dod <stalkeroma1@mail.ru>
2020-08-03 17:07:00 +0300

    add tapGestureRecognizer;locate car into imageview

--- RaceGame/GameViewController.swift
+++ RaceGame/GameViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var carImageView: UIImageView!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var roadViewOutlet: UIView!
     @IBOutlet weak var transparentViewOutlet: UIView!
-    var carImageView: UIImageView?
+//    var carImageView: UIImageView?
     let lineViewWidth: CGFloat = 20
     let lineViewHeight: CGFloat = 80
     let x = 0
@@ -24,9 +25,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.locateCar("yellow_car")
-        
-        
+         locateCar()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,6 +35,15 @@ class GameViewController: UIViewController {
         })
         
         timer.fire()
+        
+        
+        let tapLeft = UITapGestureRecognizer(target: self, action: #selector(handleLeftTap))
+        tapLeft.numberOfTapsRequired = 1
+        tapLeft.numberOfTouchesRequired = 1
+        
+        let tapRight = UITapGestureRecognizer(target: self, action: #selector(handleRightTap))
+        tapRight.numberOfTouchesRequired = 1
+        tapRight.numberOfTapsRequired = 1
     }
     @IBAction func pressedPauseButton(_ sender: UIButton) {
         
@@ -68,23 +76,32 @@ class GameViewController: UIViewController {
             }
     }
     
-    func locateCar(_ car: String) {
-        
-        guard var imageViewGuard = carImageView else {
+    func locateCar() {
+        guard let centerImage: CGPoint = carImageView.center else {
             return
         }
+//        guard var imageView = carImageView else {
+//            return
+//        }
         
-        let lineViewCenterX:CGFloat = roadViewOutlet.frame.size.width / 2 - CGFloat(lineViewWidth)/2
-        let yHeight = UIScreen.main.bounds.height - imageViewGuard.bounds.height
-
-        imageViewGuard = UIImageView(frame: CGRect(x: 0, y: 400, width: 400, height: 400))
+//        let lineViewCenterX:CGFloat = roadViewOutlet.frame.size.width / 2 - CGFloat(lineViewWidth)/2
+//        let yHeight = UIScreen.main.bounds.height - imageViewGuard.bounds.height
+//
+//        imageView = UIImageView(frame: CGRect(x: 0, y: 400, width: 400, height: 400))
+        let frame = carImageView.bounds
         
+        let imageView = UIImageView(frame: frame)
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .none
+        imageView.image = Images.yellow_car
+        
+        let center = centerImage
+        imageView.center = center
 //        carImageView = UIImageView(frame: CGRect(x: 400/*lineViewCenterX*/, y: 400/*yHeight*/, width: roadViewOutlet.bounds.width / 2, height: imageViewGuard.bounds.height))
         
 //        carImageView?.image = UIImage(named: car)
-        
-        imageViewGuard.image = UIImage(named: car)
-        roadViewOutlet.addSubview(imageViewGuard)
+
+        self.roadViewOutlet.addSubview(imageView)
         
     }
     
@@ -92,7 +109,27 @@ class GameViewController: UIViewController {
         
     }
     
+    @objc func handleLeftTap(_ sender: UITapGestureRecognizer) {
+        UIImageView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            self.carImageView.layer.position = CGPoint(x: 0, y: UIScreen.main.bounds.height - self.carImageView.bounds.height)
+        })
+    }
+    
+    @objc func handleRightTap(_ sender: UITapGestureRecognizer) {
+        UIImageView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            self.carImageView.layer.position = CGPoint(x: UIScreen.main.bounds.width - self.carImageView.bounds.width, y: UIScreen.main.bounds.height - self.carImageView.bounds.height)
+        })
+    }
+    
     func moveCarToRight(){
         
     }
+    
+    func nukeAllAnimations() {
+        roadViewOutlet.subviews.forEach({$0.layer.removeAllAnimations()})
+        roadViewOutlet.layer.removeAllAnimations()
+        roadViewOutlet.layoutIfNeeded()
+    }
+    
+    
 }
